077e692a0c7edf751b94bae1e2aab645d278001e
Lazizbek <aniprouz@gmail.com>
2026-08-08 21:27:35 +0500

    database fayli o'zgartirildi

--- database/mongobase.py
+++ database/mongobase.py
@@ -5,7 +5,7 @@ from Inlinebutton import adminpaneltug
 rt = Router()
 
 
-client = MongoClient("mongodb+srv://nehot32657_user:Lazizbek_12@cluster0.29rhhhl.mongodb.net/")
+client = MongoClient("mongodb+srv://dokena9253_db_user:ECcqNdijLyrgkcEV@cluster0.p0ocptv.mongodb.net/")
 kanal = client["Kanal"]
 adminbase = client['adminbotga']
 admin_calleks = adminbase['adminlar']
